395b77ff02805988a09555cfefb23f4f058bb3a5
satrioagnag <117004344+satrioagnag@users.noreply.github.com>
2022-11-10 09:25:05 +0700

    Add files via upload

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication9;
+import java.util.Scanner;
+/**
+ *
+ * @author Diablo Scar
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("me + you?");
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+if (i == 2) {
+  System.out.println("I <3 U");
+}
+else {
+  System.out.println(" I </3 U");
+    }
+    }
+}
+
